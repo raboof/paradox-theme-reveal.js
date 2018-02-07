e614f634c2dfee8be1ee2b9534a659d0a4523ec7
Arnout Engelen <arnouten@bzzt.net>
2018-02-07 16:48:41 +0100

    scalafmt

--- plugin/src/main/scala/ParadoxRevealPlugin.scala
+++ plugin/src/main/scala/ParadoxRevealPlugin.scala
@@ -10,75 +10,86 @@ object ParadoxRevealPlugin extends AutoPlugin {
 
   override def projectSettings: Seq[Setting[_]] = Seq(
     paradoxDirectives += {
-  import com.lightbend.paradox.markdown.Writer.Context
-  import com.lightbend.paradox.markdown.WrapDirective
+      import com.lightbend.paradox.markdown.Writer.Context
+      import com.lightbend.paradox.markdown.WrapDirective
 
-  (_: Context) => new WrapDirective("section") {
-    import org.pegdown.ast._
-    import org.pegdown.Printer
+      (_: Context) =>
+        new WrapDirective("section") {
+          import org.pegdown.ast._
+          import org.pegdown.Printer
 
-    override def render(node: DirectiveNode, visitor: Visitor, printer: Printer): Unit = {
-      val id =
-        node.attributes.identifier match {
-          case null => ""
-          case x    => s""" id="$x""""
+          override def render(node: DirectiveNode,
+                              visitor: Visitor,
+                              printer: Printer): Unit = {
+            val id =
+              node.attributes.identifier match {
+                case null => ""
+                case x    => s""" id="$x""""
+              }
+            val classes =
+              node.attributes.classesString match {
+                case "" => ""
+                case x  => s""" class="$x""""
+              }
+            import scala.collection.JavaConverters._
+            val attrs = node.attributes.keys.asScala
+              .map(key => s""" $key="${node.attributes.value(key)}"""")
+              .mkString
+            printer.print(s"""<$typ$id$classes$attrs>""")
+            node.contentsNode.accept(visitor)
+            printer.print(s"</$typ>")
+          }
         }
-      val classes =
-        node.attributes.classesString match {
-          case "" => ""
-          case x  => s""" class="$x""""
+    },
+    paradoxDirectives += {
+      import com.lightbend.paradox.markdown.Writer.Context
+      import com.lightbend.paradox.markdown.WrapDirective
+
+      (_: Context) =>
+        new WrapDirective("section") {
+          import org.pegdown.ast._
+          import org.pegdown.Printer
+
+          override def render(node: DirectiveNode,
+                              visitor: Visitor,
+                              printer: Printer): Unit = {
+            val id =
+              node.attributes.identifier match {
+                case null => ""
+                case x    => s""" id="$x""""
+              }
+            val classes =
+              node.attributes.classesString match {
+                case "" => ""
+                case x  => s""" class="$x""""
+              }
+            import scala.collection.JavaConverters._
+            val attrs = node.attributes.keys.asScala
+              .map(key => s""" $key="${node.attributes.value(key)}"""")
+              .mkString
+            printer.print(s"""<$typ$id$classes$attrs>""")
+            node.contentsNode.accept(visitor)
+            printer.print(s"</$typ>")
+          }
         }
-      import scala.collection.JavaConverters._
-      val attrs = node.attributes.keys.asScala.map(key => s""" $key="${node.attributes.value(key)}"""").mkString
-      printer.print(s"""<$typ$id$classes$attrs>""")
-      node.contentsNode.accept(visitor)
-      printer.print(s"</$typ>")
+    },
+    paradoxDirectives += {
+      import com.lightbend.paradox.markdown.Writer.Context
+      import com.lightbend.paradox.markdown.InlineDirective
+
+      (_: Context) =>
+        new InlineDirective("notes") {
+          import org.pegdown.ast._
+          import org.pegdown.Printer
+
+          override def render(node: DirectiveNode,
+                              visitor: Visitor,
+                              printer: Printer): Unit = {
+            printer.print(s"""<aside class="notes">""")
+            node.contentsNode.accept(visitor)
+            printer.print(s"</aside>")
+          }
+        }
     }
-  }
-},
-
-paradoxDirectives += {
-  import com.lightbend.paradox.markdown.Writer.Context
-  import com.lightbend.paradox.markdown.WrapDirective
-
-  (_: Context) => new WrapDirective("section") {
-    import org.pegdown.ast._
-    import org.pegdown.Printer
-
-    override def render(node: DirectiveNode, visitor: Visitor, printer: Printer): Unit = {
-      val id =
-        node.attributes.identifier match {
-          case null => ""
-          case x    => s""" id="$x""""
-        }
-      val classes =
-        node.attributes.classesString match {
-          case "" => ""
-          case x  => s""" class="$x""""
-        }
-      import scala.collection.JavaConverters._
-      val attrs = node.attributes.keys.asScala.map(key => s""" $key="${node.attributes.value(key)}"""").mkString
-      printer.print(s"""<$typ$id$classes$attrs>""")
-      node.contentsNode.accept(visitor)
-      printer.print(s"</$typ>")
-    }
-  }
-},
-
-paradoxDirectives += {
-  import com.lightbend.paradox.markdown.Writer.Context
-  import com.lightbend.paradox.markdown.InlineDirective
-
-  (_: Context) => new InlineDirective("notes") {
-    import org.pegdown.ast._
-    import org.pegdown.Printer
-
-    override def render(node: DirectiveNode, visitor: Visitor, printer: Printer): Unit = {
-      printer.print(s"""<aside class="notes">""")
-      node.contentsNode.accept(visitor)
-      printer.print(s"</aside>")
-    }
-  }
-}
-)
+  )
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,3 @@
 addSbtPlugin("com.lightbend.paradox" % "sbt-paradox-theme" % "0.3.2")
+
+addSbtPlugin("com.lucidchart" % "sbt-scalafmt" % "1.15")
